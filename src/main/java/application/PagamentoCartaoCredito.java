16f2ee86abe38ae2a004ec7e75f885e48cfded13
Marcio Gabriel Schinor Mazega <marcioschinor@gmail.com>
2025-11-18 00:43:10 +0000

    Aula 17-11-2025

--- src/main/java/application/PagamentoCartaoCredito.java
+++ src/main/java/application/PagamentoCartaoCredito.java
@@ -3,14 +3,15 @@ package application;
 import lombok.Getter;
 
 @Getter
-public class PagamentoCartaoCredito extends Pagamento{
+public class PagamentoCartaoCredito extends Pagamento implements Priorizavel, Processavel {
     private String numeroCartao;
     private String titularCartao;
     private String validadeCartao;
     private String cvv;
+    private int prioridade;
 
     public PagamentoCartaoCredito(
-        double valor, String descricao, String numeroCartao, String titularCartao, String validadeCartao, String cvv
+        double valor, String descricao, String numeroCartao, String titularCartao, String validadeCartao, String cvv, int prioridade
     ) {
         super(valor, descricao);
         this.numeroCartao = numeroCartao;
@@ -25,8 +26,36 @@ public class PagamentoCartaoCredito extends Pagamento{
         return true;
     }
 
+    @Override
     public boolean validarDados() {
         System.out.println("Validando dados do cartão para transação " + this.getIdTransacao());
         return true;
+    }
+
+    @Override
+    public int obterNivelPrioridade() {
+        return this.prioridade;
+    }
+
+    @Override
+    public void definirPrioridade(int prioridade) {
+        if (prioridade >= 1 && prioridade <=10) {
+            this.prioridade = prioridade;
+        } else {
+            System.out.println("Nível de prioridade inválido (1-10) ");
+        }
+    }
+
+    @Override
+    public boolean processar() {
+        System.out.println("Processando pagamento via cartão ID: " + this.getIdTransacao());
+        System.out.println("Transação " + this.getIdTransacao() + "APROVADO");
+        this.setStatus("Aprovado");
+        return true;
+    }
+
+    @Override
+    public String obterStatusProcessamento() {
+        return "Processamento via cartão ID: " + this.getIdTransacao() + " : " + this.getStatus();
     }
 }
